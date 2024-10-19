af205fac345d454137ad064dea0dd3dfc6d3ef84
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-10-18 21:31:28 -0500

    Working by now

---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  const { data: djangoapp } = await useFetch("http://djangoapp:8000/api/post/");
+  const { data: api } = await useFetch("/api");
+</script>
+
+<template>
+  <div>
+    <span>Response from djangoapp</span>
+    <div>
+      <pre>
+                {{ djangoapp }}
+            </pre
+      >
+    </div>
+  </div>
+  <div>
+    <span>Response from api</span>
+    <div>
+      <pre>{{ api }}</pre>
+    </div>
+  </div>
+</template>
